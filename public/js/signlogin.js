758c4175c861288a98f1f4d92df9c709b5fa8cab
rvi01 <114779097+rvi01@users.noreply.github.com>
2024-02-01 21:14:10 -0600

    login data

--- public/js/signlogin.js
+++ public/js/signlogin.js
@@ -1,5 +1,6 @@
 const signup = document.getElementById('signup');
 const signupForm = document.getElementById('signupForm');
+const url = window.location.href;
 
 signup.addEventListener('click', function() {
     if (signupForm.style.display === 'none') {
@@ -18,20 +19,148 @@ signup.addEventListener('click', function() {
 });
 
 const addsignupUser = document.querySelector("#signupForm");
+const add_first_name = document.querySelector("#first_name");
+const add_last_name = document.querySelector("#last_name");
+const add_email = document.querySelector("#email");
+const add_phone_number = document.querySelector("#phone_number");
+const add_password = document.querySelector("#password");
+
 
 addsignupUser.addEventListener("submit", async (e) => {
     e.preventDefault();
-    alert("signupform")
+    const first_name  = add_first_name.value;
+    const last_name  = add_last_name.value;
+    const email  = add_email.value;
+    const phone_number  = add_phone_number.value;
+    const password  = add_password.value;
+
+    const add_data = {
+        first_name : first_name,
+        last_name : last_name,
+        email : email,
+        phone_number : phone_number,
+        password : password
+   };
+
+    try {
+        const response = await fetch('/loginUsers/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(add_data)
+        });
+        if (response.ok) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 1500,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "success",
+                title: "Signed in successfully"
+            }).then(function() {
+                window.location = url+"home";
+            });
+        } else {
+            response.text().then(errorMessage => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: errorMessage || "Something went wrong!",
+                });
+            }).catch(error => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Something went wrong!",
+                });
+            });
+        }
+    } catch (error) {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    // Handle network errors or other issues
+    }
 })
 
 const login = document.getElementById('login');
 const LogInForm = document.getElementById('LogInForm');
 
 const checkLogin = document.querySelector("#LogInForm");
+const add_loginEmail = document.querySelector("#loginEmail");
+const add_loginPassword = document.querySelector("#loginPassword");
 
 checkLogin.addEventListener("submit", async (e) => {
     e.preventDefault();
-    alert("checkLogin")
+    const loginEmail  = add_loginEmail.value;
+    const loginPassword  = add_loginPassword.value;
+
+    const login_data = {
+        loginEmail : loginEmail,
+        loginPassword : loginPassword
+    };
+
+    try {
+        const response = await fetch('/loginUsers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(login_data)
+        });
+        if (response.ok) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 1500,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "success",
+                title: "Signed in successfully"
+            }).then(function() {
+                window.location = url+"home";
+            });
+        } else {
+            console.error('Failed to submit data');
+            response.text().then(errorMessage => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: errorMessage || "Something went wrong!",
+                });
+            }).catch(error => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Something went wrong!",
+                });
+            });
+        }
+    } catch (error) {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    // Handle network errors or other issues
+    }
+    
 })
 
 login.addEventListener('click', function() {
--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
-
+const bcrypt = require('bcryptjs');
 const app = express();
+const session = require('express-session');
 const port = process.env.PORT || 3000;
 // require the router module
 const router = require("./router");
@@ -48,6 +49,49 @@ app.get("/list",async (req, res) => {
     } catch (error) {
         console.error("Error fetching data from MongoDB:", error);
         res.status(500).send("Internal Server Error");
+    }
+});
+
+app.post('/loginUsers/add',async (req, res) => {
+    const user = new User(req.body);
+    const password = req.body.password;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword
+
+    const existingUser = await User.findOne({ email: user.email });
+    if (existingUser) {
+        return res.status(400).send({ error: 'User with this email already exists' });
+    }
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+app.post('/loginUsers',async (req, res) => {
+    
+    try {
+        const loginEmail = req.body.loginEmail;
+        const user = await User.findOne({ email : loginEmail });
+        
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        const password = user.password;
+    
+        const isPasswordValid = await bcrypt.compare(req.body.loginPassword, password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        console.log("isPasswordValid =>",isPasswordValid)
+        console.log("user =>",user)
+        res.redirect('/home');
+    } catch (error) {
+        res.status(500).json({ message: 'Error logging in' });
     }
 });
 
